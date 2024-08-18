cfbb7635fafe025a21caa577c324f7e2a0e49566
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-08-18 18:06:45 +0200

    Install Composer and replace our custom autoloader with the PSR-4 Composer autoloader.

--- public/index.php
+++ public/index.php
@@ -3,18 +3,12 @@
 use Core\Session;
 use Core\ValidationException;
 
+const BASE_PATH = __DIR__ . '/../';
+require BASE_PATH . '/vendor/autoload.php';
+
 session_start();
 
-const BASE_PATH = __DIR__ . '/../';
-
 require BASE_PATH . 'Core/functions.php';
-
-spl_autoload_register(function ($class) {
-
-  $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-
-  require base_path("{$class}.php");
-});
 
 require base_path('bootstrap.php');
 
